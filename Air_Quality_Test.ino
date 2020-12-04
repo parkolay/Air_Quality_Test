0b7d7c75958701d7c586638c5eb4da8072e0af63
Parkolay <parkolay@gmail.com>
2020-12-04 15:12:43 -0500

    Update Air_Quality_Test.ino

--- Air_Quality_Test.ino
+++ Air_Quality_Test.ino
@@ -1,7 +1,6 @@
-/*
-*/
-/*  This sketch sets up a small OLED display and pulls analog signals from two sensors 
- * (HTM1735LF (RH && Temp) && MQ135(air quality --> SnO2 detects CO2 && VOC))
+*  This sketch sets up a small OLED display and pulls analog signals from two sensors 
+ * (BME280 (RH && Temp && Press) && MQ135(air quality --> SnO2 detects CO2 && VOC))
+ * an RTC has also been added to this project for later use. 
  * 
  *  Requirements:
  *
@@ -27,30 +26,51 @@
 
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include "SparkFunBME280.h"
+#include "RTClib.h"
 
 #ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
 #endif
 
+//Initialize BME280
+BME280 bme280;
+
+//Initialize SSD1306 Display
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+//Initialize RTC
+DS3231 rtc;
 
 void setup(void) 
 {
-  Serial.begin(115200);
+  Wire.begin();
+  Serial.begin(57600);
   Serial.println("initializing...");
+
+  SetupRTC();
+  
   
   u8g2.begin();
     // connect AREF to 3.3V and use that as VCC, less noisy!  
     //thank you for the tip Lady Ada
   analogReference(EXTERNAL);  
+
+  bme280.setI2CAddress(0x76); //Connect to a second sensor
+  if (bme280.beginI2C() == false) 
+    {
+      Serial.println("Sensor connect failed");
+    }
 }
 
 void loop(void) 
 {
 
-  UpdateRH();
-  UpdateTemp();
+  //UpdateRH();
+  //UpdateTemp();
   UpdateMQ135();
+  DebugBMEData();
+  UpdateRTC();
 
 
   delay(100);
